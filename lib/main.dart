58caa5e7634b54ff65e9624e1afc517fd64c505a
muhamadfarhann <mfarhan2797@gmail.com>
2024-10-01 05:52:05 +0700

    Initial commit HACKTOBERFEST

---
+++ lib/main.dart
@@ -0,0 +1,418 @@
+import 'dart:async';
+
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:confetti/confetti.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false, home: _SimpleExampleApp()));
+}
+
+class _SimpleExampleApp extends StatefulWidget {
+  const _SimpleExampleApp();
+
+  @override
+  _SimpleExampleAppState createState() => _SimpleExampleAppState();
+}
+
+class _SimpleExampleAppState extends State<_SimpleExampleApp> {
+  late AudioPlayer player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Create the audio player.
+    player = AudioPlayer();
+
+    // Set the release mode to keep the source after playback has completed.
+    player.setReleaseMode(ReleaseMode.stop);
+
+    // Start the player as soon as the app is displayed.
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await player.setSource(AssetSource('hbd.mp3'));
+      await player.resume();
+    });
+  }
+
+  @override
+  void dispose() {
+    // Release all sources and dispose the player.
+    player.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Center(
+          child: Text(
+            'Selamat Datang',
+            style: TextStyle(
+              fontFamily: 'Source Code Pro',
+              color: Colors.white,
+              fontSize: 22,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: PlayerWidget(
+        player: player,
+      ),
+    );
+  }
+}
+
+// The PlayerWidget is a copy of "/lib/components/player_widget.dart".
+//#region PlayerWidget
+
+class PlayerWidget extends StatefulWidget {
+  final AudioPlayer player;
+
+  const PlayerWidget({
+    required this.player,
+    super.key,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return _PlayerWidgetState();
+  }
+}
+
+class _PlayerWidgetState extends State<PlayerWidget> {
+  final controller = ConfettiController();
+  bool isPlaying = false;
+
+  PlayerState? _playerState;
+  Duration? _duration;
+  Duration? _position;
+
+  StreamSubscription? _durationSubscription;
+  StreamSubscription? _positionSubscription;
+  StreamSubscription? _playerCompleteSubscription;
+  StreamSubscription? _playerStateChangeSubscription;
+
+  bool get _isPlaying => _playerState == PlayerState.playing;
+
+  bool get _isPaused => _playerState == PlayerState.paused;
+
+  String get _durationText => _duration?.toString().split('.').first ?? '';
+
+  String get _positionText => _position?.toString().split('.').first ?? '';
+
+  AudioPlayer get player => widget.player;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Confetti
+    controller.addListener(() {
+      setState(() {
+        isPlaying = controller.state == ConfettiControllerState.playing;
+      });
+    });
+    //
+
+    // Use initial values from player
+    _playerState = player.state;
+    player.getDuration().then(
+          (value) => setState(() {
+            _duration = value;
+          }),
+        );
+    player.getCurrentPosition().then(
+          (value) => setState(() {
+            _position = value;
+          }),
+        );
+    _initStreams();
+  }
+
+  @override
+  void setState(VoidCallback fn) {
+    // Subscriptions only can be closed asynchronously,
+    // therefore events can occur after widget has been disposed.
+    if (mounted) {
+      super.setState(fn);
+    }
+  }
+
+  @override
+  void dispose() {
+    _durationSubscription?.cancel();
+    _positionSubscription?.cancel();
+    _playerCompleteSubscription?.cancel();
+    _playerStateChangeSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final color = Theme.of(context).primaryColor;
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          // mainAxisSize: MainAxisSize.min,
+          children: [
+            Center(
+              child: Container(
+                padding: EdgeInsets.all(20),
+                width: double.infinity,
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText(
+                        'Halloo... 👋👋',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Hari ini tanggal 7 November',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Hari ini hari ulang tahun kamu kan..',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Selamat ulang tahun yaa.. :) 🥳🥳🥳',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Semoga panjang umur 🤲🤲',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Sehat selalu.. 🤲🤲',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Dimudahkan rezekinya.. 🤲🤲',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Dimudahkan segala urusannya.. 🤲🤲',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Dimudahkan segalanyaa pokonya yaa 🤲🤲',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Doa - doa yg terbaik pokonyaa..',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                      TypewriterAnimatedText(
+                        'Sekali lagi selamat ulang tahun yaa :) 🥳🥳🥳',
+                        textStyle: TextStyle(
+                          fontFamily: 'Source Code Pro',
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                        speed: const Duration(milliseconds: 150),
+                      ),
+                    ],
+                    totalRepeatCount: 1,
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.white,
+              ),
+              child: Text(
+                isPlaying ? 'Stop' : 'Rayakan Ulang Tahun',
+                style: const TextStyle(
+                  fontFamily: 'Source Code Pro',
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              onPressed: () {
+                if (isPlaying) {
+                  print('stop');
+                  controller.stop();
+                } else {
+                  print('start');
+                  controller.play();
+                }
+              },
+            ),
+            ConfettiWidget(
+              confettiController: controller,
+              shouldLoop: true,
+              blastDirectionality: BlastDirectionality.explosive,
+              numberOfParticles: 70,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // IconButton(
+                //   key: const Key('play_button'),
+                //   onPressed: _isPlaying ? null : _play,
+                //   iconSize: 48.0,
+                //   icon: const Icon(Icons.play_arrow),
+                //   color: Colors.white,
+                // ),
+                // IconButton(
+                //   key: const Key('pause_button'),
+                //   onPressed: _isPlaying ? _pause : null,
+                //   iconSize: 48.0,
+                //   icon: const Icon(Icons.pause),
+                //   color: Colors.white,
+                // ),
+                // IconButton(
+                //   key: const Key('stop_button'),
+                //   onPressed: _isPlaying || _isPaused ? _stop : null,
+                //   iconSize: 48.0,
+                //   icon: const Icon(Icons.stop),
+                //   color: Colors.white,
+                // ),
+              ],
+            ),
+          ],
+        ),
+        // Slider(
+        //   onChanged: (value) {
+        //     final duration = _duration;
+        //     if (duration == null) {
+        //       return;
+        //     }
+        //     final position = value * duration.inMilliseconds;
+        //     player.seek(Duration(milliseconds: position.round()));
+        //   },
+        //   value: (_position != null &&
+        //           _duration != null &&
+        //           _position!.inMilliseconds > 0 &&
+        //           _position!.inMilliseconds < _duration!.inMilliseconds)
+        //       ? _position!.inMilliseconds / _duration!.inMilliseconds
+        //       : 0.0,
+        // ),
+        // Text(
+        //   _position != null
+        //       ? '$_positionText / $_durationText'
+        //       : _duration != null
+        //           ? _durationText
+        //           : '',
+        //   style: const TextStyle(fontSize: 16.0),
+        // ),
+      ],
+    );
+  }
+
+  void _initStreams() {
+    _durationSubscription = player.onDurationChanged.listen((duration) {
+      setState(() => _duration = duration);
+    });
+
+    _positionSubscription = player.onPositionChanged.listen(
+      (p) => setState(() => _position = p),
+    );
+
+    _playerCompleteSubscription = player.onPlayerComplete.listen((event) {
+      setState(() {
+        _playerState = PlayerState.stopped;
+        _position = Duration.zero;
+      });
+    });
+
+    _playerStateChangeSubscription =
+        player.onPlayerStateChanged.listen((state) {
+      setState(() {
+        _playerState = state;
+      });
+    });
+  }
+
+  Future<void> _play() async {
+    await player.resume();
+    setState(() => _playerState = PlayerState.playing);
+  }
+
+  Future<void> _pause() async {
+    await player.pause();
+    setState(() => _playerState = PlayerState.paused);
+  }
+
+  Future<void> _stop() async {
+    await player.stop();
+    setState(() {
+      _playerState = PlayerState.stopped;
+      _position = Duration.zero;
+    });
+  }
+}
+
+//#endregion
